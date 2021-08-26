7f03a87b523e14f65ac47bf3f0279cad609d4f6c
Flytre <flytre7@gmail.com>
2021-08-26 13:26:28 -0400

    First commit

---
+++ public/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/nodejs
+
+// -------------- load packages -------------- //
+// INITIALIZATION STUFF
+
+var express = require('express')
+var app = express();
+
+console.log('class object app created');
+
+// -------------- express 'get' handlers -------------- //
+// These 'getters' are what fetch your pages
+
+app.get('/', function(req, res){
+    console.log('default landing page accessed!');
+    res.send('hola');
+});
+
+app.get('/foo', function(req, res){
+    res.send('requested foo');
+});
+
+app.get('/not_a_search', function(req, res){
+    var theQuery = req.query.q;
+    res.send('query parameter:' + theQuery);
+});
+
+
+
+// -------------- listener -------------- //
+// // The listener is what keeps node 'alive.' 
+
+var listener= app.listen(process.env.PORT || 8080, process.env.HOST || "0.0.0.0", function() {
+    console.log("Express server started");
+});
